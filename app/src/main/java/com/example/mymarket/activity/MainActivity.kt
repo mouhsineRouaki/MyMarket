253ca18f201ce5ad5033efe0a3c048e6e81bcb0f
AnassLoriga <lorigaanas1@gmail.com>
2024-12-04 18:16:29 +0100

    changeeesss

--- app/src/main/java/com/example/mymarket/activity/MainActivity.kt
+++ app/src/main/java/com/example/mymarket/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mymarket.activity
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -19,7 +20,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        window.statusBarColor = ContextCompat.getColor(this, R.color.light_blue)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.textcolor)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         setContentView(R.layout.activity_main)
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
